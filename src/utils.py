52540fd800b1533f73da634b649ede2c17cb5cf5
Vi Minh Hiển <Viminhhien1109@gmail.com>
2026-04-13 14:17:50 +0700

    update: utils.py

---
+++ src/utils.py
@@ -0,0 +1,67 @@
+import os
+import csv
+
+def read_image_paths(csv_path):
+    """ Đọc file CSV """
+
+    base_dir = os.path.dirname(csv_path)
+
+    if base_dir == "":
+        base_dir = "."
+
+    if not os.pth.exists(csv_path):
+        print(f"không tìm thấy file {csv_path}.")
+        return []
+    
+    data_list = []
+    with open(csv_path, "r", encoding="utf-8") as file:
+        # Đọc tự động theo tên cột
+        reader = csv.DictReader(file)
+
+        for row in reader:
+            img_id = row["image_id"].strip()
+            img_path_relative = row["image_path"].strip()
+            img_path = os.path.join(base_dir, img_path_relative)
+
+            data_list.append({
+                "image_id": img_id,
+                "path": img_path
+                })
+
+
+    return data_list
+
+def write_output_csv(results, output_filename = "output.csv"):
+    """ Ghi kết quả ra file CSV """
+    headers = [
+        "image_id", "qr_index",
+        "x0", "y0", "x1", "y1", 
+        "x2", "y2", "x3", "y3",
+        "content"
+    ]
+
+    with open(output_filename, mode = "w", newline = "", encoding = "utf-8") as file:
+        writer = csv.writer(file)
+        writer.writerow(headers)
+
+        for res in results:
+            img_id = res["image_id"]
+            qrs = res["qrs"]
+
+
+            # Với trường hợp không có QR code  
+            if len(qrs) == 0:
+                writer.writerow([img_id, "", "", "", "", "", "", "", "", "", ""])
+            else:
+                for idx, qr in enumerate(qrs):
+                    writer.writerow([
+                        img_id,
+                        idx,
+                        qr["x0"], qr["y0"], qr["x1"], qr["y1"],
+                        qr["x2"], qr["y2"], qr["x3"], qr["y3"],
+                        qr.get("content", "")
+                    ])
+
+        print(f"Kết quả đã được ghi vào {output_filename}.")
+
+
